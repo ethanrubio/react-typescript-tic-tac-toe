3a6eb0133b9f75022e104b752c01da5108b3a763
ethanrubio <ethanrubio@gmail.com>
2016-11-14 16:25:06 -0800

    Add testing coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 const { resolve } = require('path');
+const testWebpackConfig = require('./config/webpack.test.js')({env: 'test'});
 
 module.exports = config => {
-
     config.set({
         basePath: resolve(''),
 
@@ -17,58 +17,36 @@ module.exports = config => {
             'test/**/*.tsx': ['webpack', 'sourcemap']
         },
 
-        webpack: {
-            devtool: 'inline-source-map',
-            resolve: {
+        webpack: testWebpackConfig,
 
-                /*
-                * An array of extensions that should be used to resolve modules.
-                *
-                * See: http://webpack.github.io/docs/configuration.html#resolve-extensions
-                */
-                extensions: ['.ts', '.tsx', '.js', '.json'],
-
-                // An array of directory names to be resolved to the current directory
-                modules: [resolve('src'), 'node_modules'],
-
-            },
-            module: {
-                rules: [
-                    {
-                        test: /\.tsx$/,
-                        loaders: [
-                            'awesome-typescript-loader'
-                        ],
-                        exclude: [/\.(spec|e2e)\.ts$/, /node_modules/]
-                    },
-                    {
-                        test: /\.json$/,
-                        loader: 'json',
-                    },
-                ]
-            },
-            externals: {
-                'jsdom': 'window',
-                'cheerio': 'window',
-                'react/lib/ExecutionEnvironment': 'true',
-                'react/addons': 'true',
-                'react/lib/ReactContext': 'window'
-            },
+        coverageReporter: {
+            reporters:[
+                //{type: 'html', dir:'coverage/'},  // https://github.com/karma-runner/karma-coverage/issues/123
+                {type: 'text'},
+                {type: 'text-summary'}
+            ],
         },
 
-
-        webpackServer: {
-            noInfo: true //please don't spam the console when running in karma!
+        // Webpack please don't spam the console when running in karma!
+        webpackMiddleware: {
+            quiet: true,
+            stats: {
+                colors: true
+            }
         },
 
         plugins: [
             'karma-webpack',
             'karma-jasmine',
+            'karma-sourcemap-writer',
             'karma-sourcemap-loader',
+            'karma-coverage',
+            'karma-remap-istanbul',
+            'karma-spec-reporter',
             'karma-chrome-launcher',
         ],
 
-        reporters: ['progress'],
+        reporters: ['progress', 'coverage'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
@@ -77,3 +55,29 @@ module.exports = config => {
         singleRun: false,
     });
 };
+
+// old config
+// coverageReporter: {
+//     dir: 'coverage',
+//     reporters: [
+//         { type: 'text-summary' },
+//         {
+//         type: 'json',
+//         subdir: '.',
+//         file: 'coverage-final.json'
+//         }
+//     ]
+// },
+
+// remapIstanbulReporter: {
+//     src: 'coverage/coverage-final.json',
+//     reports: {
+//         lcovonly: 'coverage/lcov.info',
+//         html: 'coverage/report'
+//     },
+//     timeoutNotCreated: 5000,
+//     timeoutNoMoreFiles: 1000
+// },
+
+
+// reporters: ['progress', 'coverage'], // 'karma-remap-istanbul' readd once fixed by github PRs
