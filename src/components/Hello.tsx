0ace3cfccd5dfe1e4820938334fdb7c23f8b2c4e
ethanrubio <ethanrubio@gmail.com>
2016-11-14 20:06:32 -0800

    Refactor to use stateless component

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,9 +1,14 @@
-import * as React from 'react';
+import * as React from "react";
 
-export interface HelloProps { compiler: string; framework: string; }
+interface IHelloProps { compiler: string; framework: string; }
 
-export class Hello extends React.Component<HelloProps, {}> {
-    render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
-    }
-}
+const Hello: React.StatelessComponent<IHelloProps> = ({compiler, framework}) => (
+  <h1>Hello from {compiler} and {framework}!</h1>
+);
+
+Hello.propTypes = {
+  compiler: React.PropTypes.string.isRequired,
+  framework: React.PropTypes.string.isRequired,
+};
+
+export default Hello;
--- test/components/Hello.tsx
+++ test/components/Hello.tsx
@@ -1,15 +1,15 @@
-import { shallow, render } from 'enzyme';
+import { render, shallow } from "enzyme";
 
-import * as React from 'react';
+import * as React from "react";
 
-import { Hello } from '../../src/components/Hello';
+import Hello from "../../src/components/Hello";
 
-describe('Hello Component', () => {
-  it('should create a container', () => {
+describe("Hello Component", () => {
+  it("should create a container", () => {
     const container = shallow(
       <Hello compiler="Typescript" framework="React" />);
 
     expect(container.length).toBeGreaterThan(0);
-    expect(container.text()).toBe('Hello from Typescript and React!');
+    expect(container.text()).toBe("Hello from Typescript and React!");
   });
 });
